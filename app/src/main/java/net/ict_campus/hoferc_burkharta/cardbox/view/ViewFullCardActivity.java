217f4d489af74aee5b7b24c65c9a3c0f330e8eec
C-Hofer <carla.hofer@hotmail.ch>
2016-06-15 15:57:07 +0200

    2 Push - GUI [MainActivityMenu angepasst]

--- app/src/main/java/net/ict_campus/hoferc_burkharta/cardbox/view/ViewFullCardActivity.java
+++ app/src/main/java/net/ict_campus/hoferc_burkharta/cardbox/view/ViewFullCardActivity.java
@@ -2,14 +2,21 @@ package net.ict_campus.hoferc_burkharta.cardbox.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import net.ict_campus.hoferc_burkharta.cardbox.R;
 
 public class ViewFullCardActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_full_card);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
